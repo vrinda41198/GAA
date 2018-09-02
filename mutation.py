1cf141dfd2323b4cf25a13f94040cb2fc6990fd0
Mansi1301 <mansivijay1301@gmail.com>
2018-09-02 21:27:41 +0530

    second commit

--- mutation.py
+++ mutation.py
@@ -1,13 +1,17 @@
+import random
+import copy
+
 def mutation(permutation):
-    n = len(permutation)
+    n = len(permutation[0])
     rnd = random.random()
     mutation_prob = 0.8
     if rnd < mutation_prob:
-        loci1 = random.randint(0, n - 1)
-        loci2 = random.randint(0, n - 1)
+        loci1 = random.randint(0, n - 2)
+        loci2 = random.randint(0, n - 2)
         while loci2 == loci1:
-            loci2 = random.randint(0, n - 1)
+            loci2 = random.randint(0, n - 2)
         result = copy.deepcopy(permutation)
-        result[loci1], result[loci2] = result[loci2], result[loci1]
+        result[0][loci1], result[0][loci2] = result[0][loci2], result[0][loci1]
+        result[1][loci1], result[1][loci2] = result[1][loci2], result[1][loci1]
         return result
     return permutation
