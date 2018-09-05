e8073c0acd89600959ad8a154197b45803a78c58
saloni-1999 <chauhansaloni10@gmail.com>
2018-09-05 23:17:25 +0530

    Merge branch 'master' of https://github.com/vrinda41198/GAA
    # Conflicts:
    #	.idea/gen.iml

--- nQueens.py
+++ nQueens.py
@@ -196,3 +196,21 @@ def main():
 
         
 main()
+
+
+def mutation(population: list, mutation_prob: float, n: int) -> list:
+    """"
+    :param population: Chromosome post recombination
+    :param mutation_prob: Mutation probability
+    :param n: The total number of items available
+    :return: Mutated chromosome
+    """
+    if random.randint(0, 10) < mutation_prob*10:
+        for i in range(0, len(population)):
+            x = random.randint(0, n-1)
+            if population[i][x]==0:
+                population[i][x]=1
+            else:
+                population[i][x]=0
+
+    return population
