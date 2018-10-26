12d294e3fb763ce818c7d85d4ee57f5e122e15bd
vrinda41198 <vrinda.keepsmiling@gmail.com>
2018-10-26 15:30:03 +0530

    Symbolic Regression Structure added.

---
+++ Symbolic_Regression.py
@@ -0,0 +1,43 @@
+def randomxy(x: list, y: list):
+    """
+    To generate 100 random values of x and y
+    :param x:
+    :param y:
+    :return:
+    """
+    pass
+
+
+def parse_gen(chrom: list, func_set: dict, term_set: dict):
+    """
+    To generate one parse tree
+    :param chrom:
+    :param func_set:
+    :param term_set:
+    :return:
+    """
+    pass
+
+
+def pop_gen(func_set: dict, term_set: dict):
+    """
+    To generate  thousand random parse trees
+    :param func_set:
+    :param term_set:
+    :return:
+    """
+    pass
+
+
+def fitness_evaluation(chrom: list, x: list, y: list) -> float:
+    """
+    To evaluate fitness value of each parse tree
+    :param chrom:
+    :param x:
+    :param y:
+    :return:
+    """
+    pass
+
+
+def selection()
