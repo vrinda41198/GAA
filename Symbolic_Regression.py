0faf8e8fd9f3b91badcd8be8d97988914c2e17ab
monika-blue <16ucs109@lnmiit.ac.in>
2018-10-29 18:04:27 +0530

    recombination method added to symbolic regression.

--- Symbolic_Regression.py
+++ Symbolic_Regression.py
@@ -1,3 +1,5 @@
+import random
+
 def randomxy(x: list, y: list):
     """
     To generate 100 random values of x and y
@@ -38,6 +40,40 @@ def fitness_evaluation(chrom: list, x: list, y: list) -> float:
     :return:
     """
     pass
+
+def parent_selection(population: list, x: int, y: int) -> list
+    """
+    To select parents for crossover using overselection
+    :param population:
+    :param x:
+    :param y:
+    """
+    group1 = []
+    group2 = []
+    
+
+
+
+
+def subtree_recomb(parents: list, recomb_prob: float) -> list:
+    """
+    To generate children after applying sub-tree crossover on parents
+    :param parents:
+    :param recomb_prob:
+    :param n:
+    """
+    index = []
+    temp = []
+
+    for i in range(0, 2):
+        index[i] = random.randint(0, len(parents[i]))
+        temp[i] = parents[index[i]:]
+
+    for j in range(0, 2):
+        del parents[j][index[j]:]
+        parents[j].append(temp[(j+1)%2])
+
+    return parents
 
 
 def selection() :
